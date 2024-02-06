4d705c15c92adcefcaeb10ef824c16810716e66b
trian damai <triannurizkillah@gmail.com>
2024-02-06 09:06:00 +0700

    create url file

--- repositories/profile/profile-repository.go
+++ repositories/profile/profile-repository.go
@@ -89,7 +89,7 @@ func (repo *ProfileRespository) UpdateProfilePicture(sessionId string, profilePi
 	//jika belum ada buat data baru
 	if err := repo.db.Where("user_id = ? AND key = 'profile-picture'", userId).First(&userProfile).Error; err != nil {
 		var profilePictureID = uuid.NewString()
-		userProfile := entity.UserProfile{
+		newProfile := entity.UserProfile{
 			ID:        profilePictureID,
 			Key:       "profile-picture",
 			Value:     profilePicture,
@@ -98,7 +98,7 @@ func (repo *ProfileRespository) UpdateProfilePicture(sessionId string, profilePi
 			UpdatedAt: time.Now(),
 			Deleted:   false,
 		}
-		repo.db.Save(userProfile)
+		repo.db.Create(newProfile)
 		return response.Success("", "Berhasil merubah foto profil.")
 	}
 	//update data lama
@@ -127,7 +127,7 @@ func (repo *ProfileRespository) UpdateProfileTopics(sessionId string, topics str
 	//jika belum ada buat data baru
 	if err := repo.db.Where("user_id = ? AND key = 'topics'", userId).First(&userProfile).Error; err != nil {
 		var profilePictureID = uuid.NewString()
-		userProfile := entity.UserProfile{
+		newProfile := entity.UserProfile{
 			ID:        profilePictureID,
 			Key:       "topics",
 			Value:     topics,
@@ -136,7 +136,7 @@ func (repo *ProfileRespository) UpdateProfileTopics(sessionId string, topics str
 			UpdatedAt: time.Now(),
 			Deleted:   false,
 		}
-		repo.db.Save(userProfile)
+		repo.db.Create(newProfile)
 		return response.Success("", "Berhasil merubah foto profil.")
 	}
 
@@ -166,7 +166,7 @@ func (repo *ProfileRespository) UpdateProfileLevel(sessionId string, level strin
 	//jika belum ada buat data baru
 	if err := repo.db.Where("user_id = ? AND key = 'level'", userId).First(&userProfile).Error; err != nil {
 		var profilePictureID = uuid.NewString()
-		userProfile := entity.UserProfile{
+		newProfile := entity.UserProfile{
 			ID:        profilePictureID,
 			Key:       "level",
 			Value:     level,
@@ -175,7 +175,7 @@ func (repo *ProfileRespository) UpdateProfileLevel(sessionId string, level strin
 			UpdatedAt: time.Now(),
 			Deleted:   false,
 		}
-		repo.db.Save(userProfile)
+		repo.db.Create(newProfile)
 		return response.Success("", "Berhasil menyimpan topic")
 	}
 
