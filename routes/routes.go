36fd958d05bc24ad60dea692e6d2191f7d2514c3
trian damai <triannurizkillah@gmail.com>
2024-03-03 22:00:18 +0700

    feat: add get profile

--- routes/routes.go
+++ routes/routes.go
@@ -13,8 +13,17 @@ func InitRoutes(router *gin.Engine) {
 		v1.POST("/update-profile-picture", middlewares.AuthMiddleware(), profile.UpdateProfilePicture)
 		v1.POST("/update-profile-interest-topics", middlewares.AuthMiddleware(), profile.UpdateProfileInterestTopics)
 		v1.POST("/update-profile-level", middlewares.AuthMiddleware(), profile.UpdateProfileLevel)
-		v1.GET("/users", profile.DetailUserResponse)
-		v1.GET("/profile", middlewares.AuthMiddleware(), profile.GetDetailProfile)
+		v1.POST("/update-profile")
 
+		v1.GET("/get-detail-user", middlewares.AuthMiddleware(), profile.DetailUserResponse)
+		v1.GET("/profile", middlewares.AuthMiddleware(), profile.GetDetailProfile)
+	}
+
+	v1Admin := router.Group("/v1/admin")
+	{
+		v1Admin.GET("/get-profile", middlewares.AuthMiddleware(), profile.GetDetailProfile)
+		v1Admin.GET("/get-statistic")
+		v1Admin.GET("/get-top-user")
+		v1Admin.GET("/")
 	}
 }
