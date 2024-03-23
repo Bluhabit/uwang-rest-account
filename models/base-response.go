ee0b0914e5596dcde112e448a1b384e76d9cd662
azkifairuz <azkiajmal@gmail.com>
2024-03-23 23:57:24 +0700

    fix: response get list user

--- models/base-response.go
+++ models/base-response.go
@@ -9,9 +9,9 @@ type BaseResponseWithPagination[T any] struct {
 	StatusCode uint16 `json:"status_code"`
 	Data       T      `json:"data"`
 	Message    string `json:"message"`
-	Page       uint32 `json:"page"`
-	Size       uint32 `json:"size"`
-	TotalData  uint32 `json:"total_data"`
+	Page       string `json:"page"`
+	Size       string `json:"size"`
+	TotalData  int `json:"total_data"`
 }
 
 func (BaseResponse[T]) Success(data T, message string) BaseResponse[T] {
@@ -22,9 +22,21 @@ func (BaseResponse[T]) Success(data T, message string) BaseResponse[T] {
 	}
 }
 
-func (BaseResponseWithPagination[T]) SuccessWithPagination(data T, message string, page uint32, size uint32, total_data uint32) BaseResponseWithPagination[T] {
+func (BaseResponseWithPagination[T]) SuccessWithPagination(data T, message string, page string, size string, total_data int) BaseResponseWithPagination[T] {
 	return BaseResponseWithPagination[T]{
 		StatusCode: 200,
+		Data:       data,
+		Message:    message,
+		Page:       page,
+		Size:       size,
+		TotalData:  total_data,
+	}
+
+}
+
+func (BaseResponseWithPagination[T]) BadRequestPagination(data T, message string, page string, size string, total_data int) BaseResponseWithPagination[T] {
+	return BaseResponseWithPagination[T]{
+		StatusCode: 400,
 		Data:       data,
 		Message:    message,
 		Page:       page,
