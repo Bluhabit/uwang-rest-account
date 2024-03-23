67a1fd259793b2fe96cb78fb3c3b4b7696fd2235
azkifairuz <azkiajmal@gmail.com>
2024-03-23 23:31:34 +0700

    feat: create base response with pagination

--- models/base-response.go
+++ models/base-response.go
@@ -5,12 +5,31 @@ type BaseResponse[T any] struct {
 	Data       T      `json:"data"`
 	Message    string `json:"message"`
 }
+type BaseResponseWithPagination[T any] struct {
+	StatusCode uint16 `json:"status_code"`
+	Data       T      `json:"data"`
+	Message    string `json:"message"`
+	Page       uint32 `json:"page"`
+	Size       uint32 `json:"size"`
+	TotalData  uint32 `json:"total_data"`
+}
 
 func (BaseResponse[T]) Success(data T, message string) BaseResponse[T] {
 	return BaseResponse[T]{
 		StatusCode: 200,
 		Data:       data,
 		Message:    message,
+	}
+}
+
+func (BaseResponseWithPagination[T]) SuccessWithPagination(data T, message string, page uint32, size uint32, total_data uint32) BaseResponseWithPagination[T] {
+	return BaseResponseWithPagination[T]{
+		StatusCode: 200,
+		Data:       data,
+		Message:    message,
+		Page:       page,
+		Size:       size,
+		TotalData:  total_data,
 	}
 }
 
